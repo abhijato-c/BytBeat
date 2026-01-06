ed6ac35c066f2ce98fcb23c2d3174ff698a80735
Abhijato Chatterjee <abhijato.c@gmail.com>
2026-01-06 13:33:31 -0500

    Delete old files

--- old/ImgAdder.py
+++
@@ -1,6 +0,0 @@
-import os
-
-path='C:/Users/Abhijato/Music Center/'
-
-for song in ['Kimi no na wa-Sparkle.mp4', 'The Days.mp4']:
-    os.system(f'exiftool "-CoverArt<=imgs/{song[:-4]}.jpg" "{path}{song[:-4]}.m4a" -overwrite_original')
